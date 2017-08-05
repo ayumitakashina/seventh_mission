2aa70359049b590229a41d4156e4ce36a78dfc71
AyumiTakashina <moira.horizon@gmail.com>
2017-08-05 08:15:54 +0000

    第7回課題用保険

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  devise_for :users
+  as :user do 
+  	get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  	put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+  resources :bicycles
+  root to: 'bicycles#index'
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
